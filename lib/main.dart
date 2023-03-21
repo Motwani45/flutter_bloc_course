6479b6762325fc1abe139cc9440e16d8a9f2e305
Jai Motwani <jmotwani@asite.com>
2023-03-21 12:13:58 +0530

    Boiler plate Code for App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
-import 'dart:math' as math show Random;
 
 void main() {
   runApp(const MyApp());
@@ -22,72 +20,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const names = ['Foo', 'Bar', 'Baz'];
 
-extension RandomElement<T> on Iterable<T> {
-  T getRandomElement() => elementAt(math.Random().nextInt(length));
-}
-
-class NamesCubit extends Cubit<String?> {
-  NamesCubit() : super(null);
-
-  void pickRandomName() {
-    emit(names.getRandomElement());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late final NamesCubit cubit;
-
-  @override
-  void initState() {
-    super.initState();
-    cubit = NamesCubit();
-
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    cubit.close();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Bloc course"),
-      ),
-      body: StreamBuilder<String?>(
-        builder: (context, streamData) {
-          final button = TextButton(
-            onPressed: () {
-              cubit.pickRandomName();
-            },
-            child: Text("Pick Random Name"),
-          );
-          switch (streamData.connectionState) {
-            case ConnectionState.none:
-              return button;
-            case ConnectionState.waiting:
-              return button;
-            case ConnectionState.active:
-              return Column(children: [
-                Text(streamData.data??''),
-                button,
-              ]);
-            case ConnectionState.done:
-              return const SizedBox();
-          }
-        },
-        stream: cubit.stream,
       ),
     );
   }
